44cc2bd2398d3ece4feede7a8431eb86e38582db
TwainJ <jason@2jdevelopment.com>
2015-06-06 18:27:08 -0700

    Fix exception in DomXmlWriter when writing out a non-abstract element that has a SubstitutionGroupChildRule for it.
    Effectively, the DomXmlWriter was requiring that a substitution take place for the element. But according to the standard, it is acceptable for the original element to be used (if it is not abstract).
    Tests included.

--- Framework/Atf.Core/Properties/AssemblyInfo.cs
+++ Framework/Atf.Core/Properties/AssemblyInfo.cs
@@ -39,3 +39,4 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Atf.Gui")]
 [assembly: InternalsVisibleTo("Atf.Gui.WinForms")]
 [assembly: InternalsVisibleTo("Atf.Gui.Wpf")]
+[assembly: InternalsVisibleTo("UnitTests")]
