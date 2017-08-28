8f4f85a45812294250afc5e8bfbc3570c0c474f4
Sovkd <klsc,lkds@cds,klc.Com>
2017-08-28 14:47:07 +0200

    Add the AutomationPeer to the WPF custom docking controls.

---
+++ Framework/Atf.Gui.Wpf/Docking/Automation/DockPanelAutomationPeer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Automation.Peers;
+
+namespace Sce.Atf.Wpf.Docking.Automation
+{
+    /// <summary>
+    /// Exposes <see cref="DockPanel"/> types to UI Automation.
+    /// </summary>
+    public class DockPanelAutomationPeer : FrameworkElementAutomationPeer
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="owner">The owner.</param>
+        public DockPanelAutomationPeer(FrameworkElement owner) : base(owner)
+        {
+        }
+
+        #region Override Members
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override string GetClassNameCore()
+        {
+            return nameof(DockPanel);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override List<AutomationPeer> GetChildrenCore()
+        {
+            // Variables
+            var automationPeers = new List<AutomationPeer>();
+
+            // Owner
+            var owner = Owner as DockPanel;
+            if (owner == null)
+            {
+                return automationPeers;
+            }
+
+            // Children
+            var children = owner.FindVisualChildren<DockedWindow>();
+            if (children == null)
+            {
+                return automationPeers;
+            }
+
+            var dockedWindows = children as IList<DockedWindow> ?? children.ToList();
+
+            automationPeers.AddRange(dockedWindows.Where(child => child != null).Select(CreatePeerForElement).Where(peer => peer != null));
+
+            return automationPeers;
+        }
+        #endregion
+    }
+}
---
+++ Framework/Atf.Gui.Wpf/Docking/Automation/DockedWindowAutomationPeer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace Sce.Atf.Wpf.Docking.Automation
+{
+    public class DockedWindowAutomationPeer : FrameworkElementAutomationPeer
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="owner">The owner.</param>
+        public DockedWindowAutomationPeer(FrameworkElement owner) : base(owner)
+        {
+        }
+
+        #region Override Members
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override string GetClassNameCore()
+        {
+            return nameof(DockedWindow);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override List<AutomationPeer> GetChildrenCore()
+        {
+            // Variables
+            var automationPeers = new List<AutomationPeer>();
+
+            // Owner
+            var owner = Owner as DockedWindow;
+            if (owner == null)
+            {
+                return automationPeers;
+            }
+
+            // ContentPanel
+            var contentControl = owner.FindVisualChild<ContentControl>();
+            if (contentControl == null)
+            {
+                return automationPeers;
+            }
+
+            // Content
+            var content = contentControl?.Content as UIElement;
+            if (content == null)
+            {
+                return automationPeers;
+            }
+
+            // Peers
+            var peer = CreatePeerForElement(content);
+            if (peer == null)
+            {
+                return automationPeers;
+            }
+
+            automationPeers.Add(peer);
+
+            return automationPeers;
+        }
+        #endregion
+    }
+
+}
