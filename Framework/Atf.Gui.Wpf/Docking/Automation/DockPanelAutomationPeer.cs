a12615b5ad8d349631d4beaae594db04b535a567
Sovkd <klsc,lkds@cds,klc.Com>
2017-08-29 17:13:31 +0200

    [AUTOMATION] Remove the overriden methods GetChildrenCore().

--- Framework/Atf.Gui.Wpf/Docking/Automation/DockPanelAutomationPeer.cs
+++ Framework/Atf.Gui.Wpf/Docking/Automation/DockPanelAutomationPeer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Automation.Peers;
 
 namespace Sce.Atf.Wpf.Docking.Automation
@@ -26,36 +24,6 @@ namespace Sce.Atf.Wpf.Docking.Automation
         protected override string GetClassNameCore()
         {
             return nameof(DockPanel);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        protected override List<AutomationPeer> GetChildrenCore()
-        {
-            // Variables
-            var automationPeers = new List<AutomationPeer>();
-
-            // Owner
-            var owner = Owner as DockPanel;
-            if (owner == null)
-            {
-                return automationPeers;
-            }
-
-            // Children
-            var children = owner.FindVisualChildren<DockedWindow>();
-            if (children == null)
-            {
-                return automationPeers;
-            }
-
-            var dockedWindows = children as IList<DockedWindow> ?? children.ToList();
-
-            automationPeers.AddRange(dockedWindows.Where(child => child != null).Select(CreatePeerForElement).Where(peer => peer != null));
-
-            return automationPeers;
         }
         #endregion
     }
--- Framework/Atf.Gui.Wpf/Docking/Automation/DockedWindowAutomationPeer.cs
+++ Framework/Atf.Gui.Wpf/Docking/Automation/DockedWindowAutomationPeer.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Automation.Peers;
-using System.Windows.Controls;
 
 namespace Sce.Atf.Wpf.Docking.Automation
 {
@@ -23,48 +21,7 @@ namespace Sce.Atf.Wpf.Docking.Automation
         protected override string GetClassNameCore()
         {
             return nameof(DockedWindow);
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        protected override List<AutomationPeer> GetChildrenCore()
-        {
-            // Variables
-            var automationPeers = new List<AutomationPeer>();
-
-            // Owner
-            var owner = Owner as DockedWindow;
-            if (owner == null)
-            {
-                return automationPeers;
-            }
-
-            // ContentPanel
-            var contentControl = owner.FindVisualChild<ContentControl>();
-            if (contentControl == null)
-            {
-                return automationPeers;
-            }
-
-            // Content
-            var content = contentControl?.Content as UIElement;
-            if (content == null)
-            {
-                return automationPeers;
-            }
-
-            // Peers
-            var peer = CreatePeerForElement(content);
-            if (peer == null)
-            {
-                return automationPeers;
-            }
-
-            automationPeers.Add(peer);
-
-            return automationPeers;
-        }
+        }       
         #endregion
     }
 
